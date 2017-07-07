03f6eb4c9a9720ea1ad4ae209c59d913c83c6575
JuanRodJ <juan.rodriguezjimenez@ucr.ac.cr>
2017-07-06 21:54:57 -0600

    Terminada la tabla de alcanzabilidad y opción inicial para insertar destinos

--- servidor.py
+++ servidor.py
@@ -2,13 +2,16 @@ import socket
 import os
 import os.path
 import threading
+import time
+import random
 from _datetime import datetime
 
 run = 0
 vecinos = {}
-escuchando = False;
+alcanzabilidad = {}
+alcanzabilidad['localhost'] = {}
 #esto es completamente arbitrario
-IP_HOST = "10.3.19.230"
+IP_HOST = str(random.randrange(256)) + "." + str(random.randrange(256)) + "." + str(random.randrange(256)) + "." + str(random.randrange(256))
 MSK_HOST = "255.255.255.0"
 SA_HOST = "2"
 
@@ -53,8 +56,7 @@ def escucha():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("", 57809))
     s.listen(1)
-    #creación de tabla alcanzabilidad
-    alcanzabilidad = {}
+
     while True:
         print("")
         print ("Esperando mensaje...")
@@ -104,17 +106,34 @@ def escucha():
             sa = recibido[2:6]
             ip = recibido[6:14]
             tam = recibido[14:22]
-            alcanzabilidad[ip] = {'tipo' : tipo, 'sa' : sa, 'Alcanzables': tam}
+            alcanzabilidad.update({ip: {}})
+            #alcanzabilidad[ip] = {'tipo' : tipo, 'sa' : sa, 'Alcanzables': tam}
             num = 0
-            alcanzables = alcanzabilidad[ip]
+            pos = 22
+            vectorR = alcanzabilidad[ip]
+            #alcanzables = alcanzabilidad[ip]
             for i in range (0, int(tam)):
-                alcanzables['alcanzable' + str(num)] = recibido[22+(16*i):22+(16*i)+8]
-                alcanzables['mascara' + str(num)] = recibido[22+(16*i)+8:22+(16*i)+16]
+
+                vectorR['Destino' + str(num)] = {'IP': recibido[pos:pos+8], 'Mascara': recibido[pos+8:pos+16], 'SAs': {}}
+                #alcanzables['alcanzable' + str(num)] = recibido[pos+(16*i):pos+(16*i)+8]
+                #alcanzables['mascara' + str(num)] = recibido[pos+(16*i)+8:pos+(16*i)+16]
+                destData = vectorR['Destino' + str(num)]
+                alcanzables = destData['SAs']
+                pos2 = pos+16
+                numSas = int(recibido[pos2:pos2 + 4])
+                print("NUM: " + str(numSas))
+                pos2 = pos2 + 4
+                saID = 0
+                #sistemas = alcanzables['alcanzable' + str(num)]
+                for j in range(0, int(numSas)):
+                    alcanzables['SA' + str(saID)] = recibido[pos2+(4*j):pos2+(4*j)+4]
                 num += 1
-            num = 22+(16*(int(tam)-1))+16
-            print("num", num)
-            alcanzables['SAs'] = recibido[num:num+4]
-            alcanzables['SA0'] = recibido[num+4:num+8]
+                pos = pos2+(4*(int(numSas)-1))+4
+                #pos = (pos+(16*i)+16)+4+(
+            num = pos+(16*(int(tam)-1))+16
+            #print("num", num)
+            #alcanzables['SAs'] = recibido[num:num+4]
+            #alcanzables['SA0'] = recibido[num+4:num+8]
             print("tabla alcanzabilidad: ", alcanzabilidad)
     sc.close()
     s.close()
@@ -129,6 +148,57 @@ def menu():
     print ("\t2 - Solicitar desconexión")
     print ("\t3 - Enviar información de alcanzabilidad")
     print ("\t9 - salir")
+
+os.system('clear')
+print("Se necesita agregar destinos alcanzables (digite 0 para terminar)")
+
+curDest = 0
+while True:
+    ip = input("Ingrese la dirección IP del destino\n")
+    if ip == "0":
+        break
+    else:
+        ip = ip.split(".")
+        for i in range(0,len(ip)):
+            ip[i] = hex(int(ip[i]))[2:]
+            if len(ip[i]) == 1:
+                ip[i] = "0"+ip[i]
+        ip = ''.join(ip)
+        ip = ip.encode()
+        print(ip)
+        mascara = input("Ingrese la máscara del destino\n")
+        mascara = mascara.split(".")
+        for i in range(0,4):
+            mascara[i] = hex(int(mascara[i]))[2:]
+            if len(mascara[i]) == 1:
+                mascara[i] = "0"+mascara[i]
+        mascara = ''.join(mascara)
+        mascara = mascara.encode()
+        print(mascara)
+        #alcanzabilidad['localhost'] = {'Destino' + str(curDest): {}}
+        vectorR = alcanzabilidad['localhost']
+        vectorR['Destino' + str(curDest)] = {'IP': ip, 'Mascara': mascara, 'SAs': {}}
+        destData = vectorR['Destino' + str(curDest)]
+        alcanzables = destData['SAs']
+        num = 0
+        while True:
+            sa = input("Ingrese los sistemas autónomos a seguir para llegar a este destino (0 para terminar)\n")
+            if sa == "0":
+                break
+            else:
+                sa = hex(int(sa))[2:]
+                alcanzables['Sa' + str(num) ] = sa
+                l = len(sa)
+                l = 4-l
+                for i in range(0,l):
+                    sa = "0"+sa
+                sa = sa.encode()
+                print(sa)
+                num +=1
+        curDest+=1
+
+print("Alcanzables desde el principio: ", alcanzabilidad)
+time.sleep(5)
 
 while True:
     menu()
